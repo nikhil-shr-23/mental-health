a95d436ae3a7d1340c96af2b76da943c1165776d
Nikhil Sharma <96628379+nikhil-shr-23@users.noreply.github.com>
2025-04-21 06:00:35 +0530

    Enabled Strict Mode

--- chatbotty/src/main.jsx
+++ chatbotty/src/main.jsx
@@ -1,10 +1,10 @@
-
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </>
+  </React.StrictMode>
 )
