20f6396d7c8419e97ef334ceaf0742f15658245a
Nikhil Sharma <96628379+nikhil-shr-23@users.noreply.github.com>
2025-04-21 06:06:36 +0530

    Update vite.config.js

--- chatbotty/vite.config.js
+++ chatbotty/vite.config.js
@@ -4,11 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     minify: true,
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  
   }
 })
-
-//Added Build configuration
